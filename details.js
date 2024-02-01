2ba7aafc601c5cec5418896750a04712dcd455be
RaghadHanon <raghadhanoon2015@gmail.com>
2024-02-02 00:04:22 +0200

    j

--- details.js
+++ details.js
@@ -23,23 +23,20 @@ async function detailedData() {
   document.querySelector(".product_item .swiper .swiper-wrapper").innerHTML = result;
   
   const swiper = new Swiper('.swiper', {
-    // Optional parameters
+  
     direction: 'horizontal',
     loop: true,
-
     slidesPerView:1,
-    // If we need pagination
+    autoplay:true,
     pagination: {
       el: '.swiper-pagination',
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
